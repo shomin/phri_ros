5c0c5d8e2f619062defae9dccd30308cd4ec0446
Michael Shomin <mshomin@andrew.cmu.edu>
2012-11-25 21:02:20 -0500

    narrative script template and tester

---
+++ smach/scripts.py
@@ -0,0 +1,12 @@
+base = {"attention":["Hey,,,, help me","{'Hey',fg='white',bg='red'}, help me"],
+"introduction":["Yoe, this is what I am about","Yo, this is what I am about"]}
+
+auth_deep = {}
+
+auth_shal = {}
+
+like_deep = {}
+
+like_shal = {}
+
+
---
+++ smach/test_scripts.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import scripts
+
+script = scripts.base
+
+
+print "Attention Getting:"
+print "Speech: " + script['attention'][0]
+print "Screen: " + script['attention'][1]
+
